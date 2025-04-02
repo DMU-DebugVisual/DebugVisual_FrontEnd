edecf047cfd0e7fc03c0085fc6ad3a821a53dd89
윤동준 <booohooo20@gmail.com>
2025-04-02 19:03:34 +0900

    푸시 시험해 봤습니다

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "../styles/Footer.css";
 
 const Footer = () => {
-    return <footer className="footer">Footer</footer>;
+    return <footer className="footer">Footer 푸시 시험 </footer>;
 };
 
 export default Footer;
