d0dba264c324e2a1f408e3529e2899b80851e016
Danto <rlarladlstjd@gmail.com>
2025-04-04 15:42:12 +0900

    pullrequest test

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "../styles/Header.css";
 
 const Header = () => {
-    return <header className="header">Header</header>;
+    return <header className="header">Header 1</header>;
 };
 
 export default Header;
