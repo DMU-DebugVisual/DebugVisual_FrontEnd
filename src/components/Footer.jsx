d5e82f4c2ff18c771c3a3ff8ef307ef1b68aaee4
Danto <rlarladlstjd@gmail.com>
2025-04-04 15:42:12 +0900

    pullrequest test

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "../styles/Footer.css";
 
 const Footer = () => {
-    return <footer className="footer">Footer</footer>;
+    return <footer className="footer">Footer 1</footer>;
 };
 
 export default Footer;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "../styles/Sidebar.css";
 
 const Sidebar = () => {
-    return <aside className="sidebar">Sidebar</aside>;
+    return <aside className="sidebar">Sidebar 1</aside>;
 };
 
 export default Sidebar;
