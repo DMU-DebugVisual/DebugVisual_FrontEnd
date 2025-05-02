39a489e06de92bd0f2c9685db793b55b4b851318
Danto <rlarladlstjd@gmail.com>
2025-05-02 14:20:04 +0900

    reset

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-//푸시 시험임
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
