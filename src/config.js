1ae3d9f633026bc39772549c58b4e4055bd92145
csw <csw021128@gmail.com>
2025-05-18 17:51:18 +0900

    회원가입 및 로그인 기능 구현 + API 프록시 설정 추가

--- src/config.js
+++ src/config.js
@@ -1,5 +1,8 @@
+// 개발 환경에서는 프록시를 통해 우회해야 하므로 ''로 설정
+const isDev = process.env.NODE_ENV === 'development';
+
 const config = {
-    API_BASE_URL: 'http://3.38.244.234:8080',
+    API_BASE_URL: isDev ? '' : 'http://3.38.244.234:8080',
 };
 
 export default config;
